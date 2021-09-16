94f2cad672659e31cac87f5aefee34d67b86ac6e
Thitiwut Chutipongwanit <thitiwut@ifrasoft.com>
2021-09-16 10:35:15 +0700

    Add data loading

--- main.go
+++ main.go
@@ -1,42 +1,158 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/joho/godotenv"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"io/fs"
+	"io/ioutil"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
+
+// DeviceConfig represents device configuration.
+type DeviceConfig struct {
+	// Username aka device_id
+	Username string
+
+	// Password aka primaryKey
+	Password string
+
+	// JSONStPath represents JSON mc_st file path.
+	JSONStPath string
+
+	// JSONOpPath represents JSON mc_op file path.
+	JSONOpPath string
+
+	// JSONRopPath represents JSON mc_rop file path.
+	JSONRopPath string
+
+	// Topic represents MQTT topic.
+	Topic string
+}
+
+// Message represents message schema.
+type Message struct {
+	Name  string    `json:"name"`
+	Value float64   `json:"value"`
+	Time  time.Time `json:"time"`
+}
 
 func main() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 
-	brokerURL := "tcp://staging.mqtt.ifra.io:1883"
-	topic := "organization/93d818ed-ea26-4ccf-a9ef-e1852197e96f/messages"
-	username := "ecbe49d1-8b2d-4f16-afb4-3e323a74d214"
-	password := "5b76e53f-24a1-485b-8284-d55eef426234"
-
-	opts := mqtt.NewClientOptions().
-		AddBroker(brokerURL).
-		SetUsername(username).
-		SetPassword(password)
-	mqttClient := mqtt.NewClient(opts)
-
-	log.Info().
-		Str("url", brokerURL).
-		Str("username", username).
-		Msg("Connect to MQTT broker")
-
-	if token := mqttClient.Connect(); token.Wait() && token.Error() != nil {
-		log.Fatal().
-			Err(token.Error()).
-			Str("url", brokerURL).
-			Str("username", username).
-			Msg("Connect to MQTT broker failed")
+	err := godotenv.Load()
+	if err != nil {
+		if errors.Is(err, fs.ErrNotExist) {
+			log.Info().
+				Err(err).
+				Msg(".env file not found. Use shell environment variables instead")
+		} else {
+			log.Fatal().
+				Err(err).
+				Msg("Load .env file failed")
+		}
 	}
 
-	token := mqttClient.Publish(topic, 2, false, nil)
-	if token.Wait() && token.Error() != nil {
-		log.Error().
-			Err(token.Error()).
-			Msg("Publish MQTT message failed")
+	brokerURL := os.Getenv("MDG_BROKER_URL")
+
+	byteDevConfs, err := ioutil.ReadFile("device-configs.json")
+	if err != nil {
+		log.Fatal().
+			Err(err).
+			Msg("Load device-configs.json file failed")
+	}
+
+	var devConfs []DeviceConfig
+
+	err = json.Unmarshal(byteDevConfs, &devConfs)
+	if err != nil {
+		log.Fatal().
+			Err(err).
+			Msg("Unmarshal failed")
+	}
+
+	for i := 0; i < len(devConfs); i++ {
+		conf := devConfs[i]
+
+		go func() {
+			opts := mqtt.NewClientOptions().
+				AddBroker(brokerURL).
+				SetUsername(conf.Username).
+				SetPassword(conf.Password).
+				SetClientID(conf.Username)
+			mqttClient := mqtt.NewClient(opts)
+
+			log.Info().
+				Str("url", brokerURL).
+				Str("username", conf.Username).
+				Msg("Connect to MQTT broker")
+
+			if token := mqttClient.Connect(); token.Wait() && token.Error() != nil {
+				log.Fatal().
+					Err(token.Error()).
+					Str("url", brokerURL).
+					Str("username", conf.Username).
+					Msg("Connect to MQTT broker failed")
+			}
+
+			byteStMsgs, err := ioutil.ReadFile(conf.JSONStPath)
+			if err != nil {
+				log.Fatal().Err(err).Msg("Read status messages file failed")
+			}
+
+			var stMsgs []Message
+
+			err = json.Unmarshal(byteStMsgs, &stMsgs)
+			if err != nil {
+				log.Fatal().Err(err).Msg("Unmarshal status messages failed")
+			}
+
+			byteOpMsgs, err := ioutil.ReadFile(conf.JSONOpPath)
+			if err != nil {
+				log.Fatal().Err(err).Msg("Read output messages file failed")
+			}
+
+			var opMsgs []Message
+
+			err = json.Unmarshal(byteOpMsgs, &opMsgs)
+			if err != nil {
+				log.Fatal().Err(err).Msg("Unmarshal output messages failed")
+			}
+
+			byteRopMsgs, err := ioutil.ReadFile(conf.JSONOpPath)
+			if err != nil {
+				log.Fatal().Err(err).Msg("Read reject output messages file failed")
+			}
+
+			var ropMsgs []Message
+
+			err = json.Unmarshal(byteRopMsgs, &ropMsgs)
+			if err != nil {
+				log.Fatal().Err(err).Msg("Unmarshal reject output messages failed")
+			}
+
+			token := mqttClient.Publish(conf.Topic, 2, false, nil)
+			if token.Wait() && token.Error() != nil {
+				log.Error().
+					Err(token.Error()).
+					Msg("Publish MQTT message failed")
+			}
+
+			// Make the program keep running until receive an interruption signal.
+			c := make(chan os.Signal, 1)
+			signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+			// Abort the program upon receiving an interruption signal.
+			s := <-c
+			log.Info().Msgf("Received %s signal. Aborting", s)
+
+			mqttClient.Disconnect(250)
+		}()
 	}
 }
